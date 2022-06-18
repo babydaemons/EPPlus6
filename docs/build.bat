8c2b2d28a1ec998022359e3252641ad5cfe1a6f8
MORI Shingo <babydaemons@gmail.com>
2022-06-19 03:26:59 +0900

    テストを削除/.NET Framework 4.8/x64専用化

--- docs/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo 1. Download docfx from https://github.com/dotnet/docfx/releases
-echo 2. Add path to the folder where dotfx is unzipped
-
-@docfx metadata docfx.json
-@docfx build docfx.json
